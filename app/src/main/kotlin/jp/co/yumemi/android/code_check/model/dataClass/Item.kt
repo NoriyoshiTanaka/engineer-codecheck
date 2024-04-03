c5c424064aa6cf203c4c4c6ab8426a7a2041eb60
noriyoshi.tanaka <aburagirio@gmail.com>
2024-04-03 11:37:01 +0900

    [update] コメントを削除した

--- app/src/main/kotlin/jp/co/yumemi/android/code_check/model/dataClass/Item.kt
+++ app/src/main/kotlin/jp/co/yumemi/android/code_check/model/dataClass/Item.kt
@@ -6,41 +6,18 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 data class Item(
-    @SerialName("forks_count")//
+    @SerialName("forks_count")
     val forksCount: Int,
-    @SerialName("full_name")//
+    @SerialName("full_name")
     val fullName: String,
-    @SerialName("language")//
+    @SerialName("language")
     val language: String?,
-    @SerialName("open_issues_count")//
+    @SerialName("open_issues_count")
     val openIssuesCount: Int,
-    @SerialName("owner")//
+    @SerialName("owner")
     val owner: Owner,
-    @SerialName("stargazers_count")//
+    @SerialName("stargazers_count")
     val stargazersCount: Int,
-    @SerialName("watchers_count")//
+    @SerialName("watchers_count")
     val watchersCount: Int,
-    /*
-                val name = jsonItem.optString("full_name")
-                val ownerIconUrl = jsonItem.optJSONObject("owner")!!.optString("avatar_url")
-                val language = jsonItem.optString("language")
-                val stargazersCount = jsonItem.optLong("stargazers_count")
-                val watchersCount = jsonItem.optLong("watchers_count")
-                val forksCount = jsonItem.optLong("forks_conut")
-                val openIssuesCount = jsonItem.optLong("open_issues_count")
-
-                items.add(
-                    Item(
-                        name = name,
-                        ownerIconUrl = ownerIconUrl,
-                        language = context.getString(R.string.written_language, language),
-                        stargazersCount = stargazersCount,
-                        watchersCount = watchersCount,
-                        forksCount = forksCount,
-                        openIssuesCount = openIssuesCount
-                    )
-                )
-            }
-
-     */
 )
